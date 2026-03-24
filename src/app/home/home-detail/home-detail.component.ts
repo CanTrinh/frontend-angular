9a24a54db9c02271cb5109af65875d9a1e828c53
CanTrinh <cannhai1995@gmail.com>
2026-03-24 15:29:06 +0700

    add changes

--- src/app/home/home-detail/home-detail.component.ts
+++ src/app/home/home-detail/home-detail.component.ts
@@ -16,58 +16,6 @@ import { SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./home-detail.component.css']
 })
 export class HomeDetailComponent implements OnInit {
-
-  /*post: any;
-
-  constructor(
-    private postService: PostService,
-    private sanitizeUrlService: SanitizeUrlService,
-    private route: ActivatedRoute,
-  ){}
-
-  ngOnInit(): void {
-    this.loadDetailPost();
-  }
-
-  loadDetailPost(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    
-    this.postService.getPost(id).subscribe({
-      next: (data) => {
-        this.post = data;
-      },
-      error: (err) => {
-        console.error('Error loading detailPost:', err);
-
-      }
-    });
-  }
-
-  isYoutube(url: string): boolean {
-  return url.includes('youtube.com') || url.includes('youtu.be');
-  }
-
-  isImage(url: string): boolean {
-    return /\.(jpg|jpeg|png|gif)$/i.test(url);
-  }
-
-  extractImg(imgString: string): string{
-    const getAfterImg = imgString.indexOf('img') + 4;
-    return this.post.mediaUrl.slice(getAfterImg);
-  }
-
-  isPdf(url: string): boolean {
-    return url.endsWith('.pdf');
-  }
-
-  isWord(url: string): boolean {
-    return url.endsWith('.doc') || url.endsWith('.docx');
-  }
-
-  safeUrl( url: string) {
-    return this.sanitizeUrlService.sanitizeUrl(url);
-  }*/
-
   post: any;
   safeYoutubeUrl: SafeResourceUrl | null = null;
   isShorts = false;
