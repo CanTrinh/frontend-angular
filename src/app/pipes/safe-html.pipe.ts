71c5fc7ce6c082942e3506abc696a1b5bed64b23
CanTrinh <cannhai1995@gmail.com>
2026-04-03 09:08:26 +0700

    show detail

--- src/app/pipes/safe-html.pipe.ts
+++ src/app/pipes/safe-html.pipe.ts
@@ -1,16 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
-@Pipe({
-  name: 'safeHtml',
-  standalone: true // Rất quan trọng nếu bạn dùng Standalone Component
-})
+@Pipe({ name: 'safeHtml',
+      standalone: true
+ })
 export class SafeHtmlPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
-
   transform(value: string): SafeHtml {
-    if (!value) return '';
-    // Lệnh này bảo Angular: "Tôi tin tưởng chuỗi HTML này, hãy render nó"
     return this.sanitizer.bypassSecurityTrustHtml(value);
   }
 }
