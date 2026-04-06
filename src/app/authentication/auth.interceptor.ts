85e5d7de3cf903d7dac48fb2808f02e938688127
CanTrinh <cannhai1995@gmail.com>
2026-04-06 22:18:13 +0700

    sua lai

--- src/app/authentication/auth.interceptor.ts
+++ src/app/authentication/auth.interceptor.ts
@@ -5,21 +5,27 @@ import { LoginService } from '../features/auth/login/login.service';
 
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
- const authService = inject(LoginService);
- 
+  const authService = inject(LoginService);
   const access_token = authService.getToken();
-  let clonedReq = req.clone({
-    withCredentials: true 
-  });
-  
-  if (req.url.includes('https://www.ctlife.xyz')){
+
+  // KIỂM TRA: Nếu request gửi đến Server của bạn (ctlife.xyz)
+  if (req.url.includes('api.ctlife.xyz')) {
+    // Tạo bản clone có withCredentials (để gửi/nhận Cookie Refresh)
+    let clonedReq = req.clone({ withCredentials: true });
+
+    // Nếu đã có Access Token thì gắn vào Header
     if (access_token) {
       clonedReq = clonedReq.clone({
-        setHeaders: { Authorization: `Bearer ${access_token}` }
+        setHeaders: { Authorization: `Bearer ${access_token.trim()}` }
       });
-      return next(clonedReq);
     }
+    
+    // Gửi bản clone (đã có Credentials và Token nếu có)
+    return next(clonedReq);
   }
+
+  // NGƯỢC LẠI: Nếu là API bên thứ ba (Thời tiết, v.v.)
+  // Gửi request gốc (Sạch sẽ, không Cookie, không Token) để tránh lỗi SameSite/CORS
   return next(req);
 };
 
