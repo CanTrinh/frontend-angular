8078ddcc721237a75e1085f6c5b6dcc92dce749f
CanTrinh <cannhai1995@gmail.com>
2026-04-08 10:22:49 +0700

    fixxxxx

--- src/app/features/reactions/reactions.component.ts
+++ src/app/features/reactions/reactions.component.ts
@@ -15,12 +15,12 @@ export class ReactionsComponent {
   @Input() currentUserReaction: any; // Nhận từ bài Post trả về
 
   reactionIcons = [
-    { type: 'LIKE', label: 'Thích', url: `${environment}/static/images/reactions/like_v1.svg` },
-    { type: 'LOVE', label: 'Yêu', url: `${environment}/static/images/reactions/love_v1.svg` },
-    { type: 'HAHA', label: 'Haha', url: `${environment}/static/images/reactions/haha_v1.svg` },
-    { type: 'WOW', label: 'Wow', url: `${environment}/static/images/reactions/wow_v1.svg` },
-    { type: 'SAD', label: 'Buồn', url: `${environment}/static/images/reactions/sad_v1.svg` },
-    { type: 'ANGRY', label: 'Tức', url: `${environment}/static/images/reactions/angry_v1.svg` },
+    { type: 'LIKE', label: 'Thích', url: `${environment.cloudFrontUrl}/static/images/reactions/like_v1.svg` },
+    { type: 'LOVE', label: 'Yêu', url: `${environment.cloudFrontUrl}/static/images/reactions/love_v1.svg` },
+    { type: 'HAHA', label: 'Haha', url: `${environment.cloudFrontUrl}/static/images/reactions/haha_v1.svg` },
+    { type: 'WOW', label: 'Wow', url: `${environment.cloudFrontUrl}/static/images/reactions/wow_v1.svg` },
+    { type: 'SAD', label: 'Buồn', url: `${environment.cloudFrontUrl}/static/images/reactions/sad_v1.svg` },
+    { type: 'ANGRY', label: 'Tức', url: `${environment.cloudFrontUrl}/static/images/reactions/angry_v1.svg` },
   ];
 
   onSelect(type: string) {
