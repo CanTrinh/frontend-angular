26e253ad1c35d4f0dde8af8424c7cac750c3017e
CanTrinh <cannhai1995@gmail.com>
2026-04-20 21:00:27 +0700

    sua

--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.prod';
+import { Observable } from 'rxjs';
 
 
 @Injectable({ providedIn: 'root' })
@@ -10,7 +11,7 @@ export class CategoryService {
 
   constructor(private http: HttpClient) {}
 
-  getCatergories() {
+  getCatergories(): Observable<any> {
     return this.http.get<any[]>(`${this.apiUrl}`);
   }
 
