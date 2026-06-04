ffacb20be78b7b2e047e8496cb02ced7c3b57e1c
CanTrinh <cannhai1995@gmail.com>
2026-06-04 18:39:07 +0700

    fix dto

--- src/app/shared/types/friendInfor.interface.ts
+++ src/app/shared/types/friendInfor.interface.ts
@@ -1,9 +1,7 @@
  export interface FriendInforDto {
-  userId: string,
-  status: string,
-  friend:{
+  
     id: string;
     name: string;
-    profilePic: string;
-  }
+    avatar: string;
+  
 }
