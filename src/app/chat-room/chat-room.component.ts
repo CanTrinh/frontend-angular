b5a3e59f0a398a0f20d79c1c2ce228ccdbd552e7
CanTrinh <cannhai1995@gmail.com>
2026-06-09 16:10:07 +0700

    lại nào

--- src/app/chat-room/chat-room.component.ts
+++ src/app/chat-room/chat-room.component.ts
@@ -257,6 +257,7 @@ export class ChatRoomComponent implements OnInit, OnDestroy{
 
   rejectCall() {
     this.incomingCallData = null;
+    this.isCalling = false;
     this.stopRingtone();
     this.videoService.leaveCall();
     // Gửi sự kiện respondCall nếu cần (như trong service bạn đã có)
