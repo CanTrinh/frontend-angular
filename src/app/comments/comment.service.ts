8e2a94d4bb834eb58565d2021b5536d896dcbb01
CanTrinh <cannhai1995@gmail.com>
2026-07-03 18:56:48 +0700

    test 3

--- src/app/comments/comment.service.ts
+++ src/app/comments/comment.service.ts
@@ -1,8 +1,9 @@
 // comment.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 import { CreateCommentDto } from './dto/createComment.dto';
 import { environment } from '../../environments/environment.prod';
+import { IS_PUBLIC_API } from '../authentication/http-context';
 
 
 @Injectable({ providedIn: 'root' })
@@ -12,7 +13,7 @@ export class CommentService {
   constructor(private http: HttpClient) {}
 
   getComments(postId: string) {
-    return this.http.get<any[]>(`${this.apiUrl}/${postId}/comments`);
+    return this.http.get<any[]>(`${this.apiUrl}/${postId}/comments`, {context: new HttpContext().set(IS_PUBLIC_API, true)});
   }
 
   createComment(postId: string, commentData: CreateCommentDto) {
