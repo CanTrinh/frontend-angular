3787966011f2665abf0dd365c202e2da951b537d
CanTrinh <cannhai1995@gmail.com>
2026-07-22 20:31:45 +0700

    test react lai

--- src/app/features/reactions/reactions.component.ts
+++ src/app/features/reactions/reactions.component.ts
@@ -12,7 +12,7 @@ import { REACTION_LIST } from '../../shared/constants/reaction.constant';
   templateUrl: './reactions.component.html',
   styleUrls: ['./reactions.component.css']
 })
-export class ReactionsComponent {
+/*export class ReactionsComponent {
   @Input() targetId: string;
   @Input() isPost: boolean;
   @Input() currentUserReaction: any; // Nhận từ bài Post trả về
@@ -44,5 +44,54 @@ export class ReactionsComponent {
     });
 
    
+  }
+}
+*/
+
+export class ReactionsComponent {
+  // Phân biệt chế độ hiển thị: 'post' (Nút thích to) hoặc 'message' (Thanh toolbar của Messenger)
+  @Input() mode: 'post' | 'message' | 'comment' = 'post';
+  
+  @Input() targetId: string;
+  @Input() reactionIcons: any[] = [/* Giữ nguyên mảng danh sách icon cũ */];
+  @Input() currentUserReaction: any = null;
+  @Input() isLogin: boolean = true;
+
+  @Output() reactionChanged = new EventEmitter<string | null>();
+  
+  // Bắn sự kiện ra ngoài khi bấm nút Reply hoặc More (Chỉ dùng cho mode 'message')
+  @Output() replyClicked = new EventEmitter<any>();
+  @Output() moreClicked = new EventEmitter<any>();
+
+  private http = inject(HttpClient);
+
+  onSelect(type: string): any {
+    if (this.currentUserReaction?.type === type) {
+      this.reactionChanged.emit(null);
+    } else {
+      this.reactionChanged.emit(type);
+
+          // Gọi API NestJS (toggleReaction) tại đây
+    const payload = {
+      targetId: this.targetId,
+      mode: this.mode,
+      type: type
+    };
+    console.log(`User clicked ${type} for ${this.targetId}`);
+
+    return this.http.post(`${environment.apiUrl}/posts/reactions`, payload).subscribe((reaction) =>{
+      console.log(`ban da tha reaction thanh cong: ${reaction}`)
+
+    });
+
+    }
+  }
+
+  onMainButtonClick(): void {
+    if (this.currentUserReaction) {
+      this.reactionChanged.emit(null);
+    } else {
+      this.reactionChanged.emit('LIKE');
+    }
   }
 }
